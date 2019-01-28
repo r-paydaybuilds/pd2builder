a8951e4b67245aa4a323c51f2724632d3db96e21
imurx <urielfontan2002@gmail.com>
2019-01-28 12:55:21 -0300

    eslint default

---
+++ .eslintrc.js
@@ -0,0 +1,25 @@
+module.exports = {
+    "env": {
+        "es6": true,
+        "node": true
+    },
+    "extends": "eslint:recommended",
+    "parserOptions": {
+        "ecmaVersion": 8,
+        "sourceType": "module"
+    },
+    "rules": {
+        "indent": [
+            "error",
+            4
+        ],
+        "quotes": [
+            "error",
+            "double"
+        ],
+        "semi": [
+            "error",
+            "always"
+        ]
+    }
+};
