79450c36d1f4a5e66572aca75d070f1da178133a
imurx <urielfontan2002@gmail.com>
2019-01-30 14:40:46 -0300

    some classy look

--- public/js/builder.gui.js
+++ public/js/builder.gui.js
@@ -1,88 +1,83 @@
 /**
- * Class-like object for management of the GUI functions. 
+ * Class object for management of the GUI functions. 
  */
-function GUI() { 
-  
-}; 
+class GUI {
+    constructor() {
 
-/** Change site title. Useful for naming builds, you can later find them easier in your history for example.
- * @param {string} titleText Title to change to.
- */
-GUI.prototype.Title_ChangeTo = function (titleText) {
-  if (titleText) {
-    $("head title").text(titleText);
-  }
-  else {
-    $("head title").text("Payday 2 Builder");
-  }
-}
-
-/**
- * Change selected Skill Tab to another.
- * @param {string} containerId Id of the Tab to switch to 
- */
-GUI.prototype.Tab_ChangeTo = function (containerId) {
-  $("#sk_container").children(".sk_container").each(function () {
-    $(this).hide(); 
-  });
-  $("#" + containerId).show(); 
-}
-
-/**
- * Raise or lower the subtree background according to points set in it.
- * Does not yet support accurate moving (only at set thresholds)
- * @param {string} subtreeId Id of the subtree to move
- * @param {number} pointsInTree Number of points to "move to"
- */
-GUI.prototype.Subtree_MoveBackground = function (subtreeId, pointsInTree) {
-  if (pointsInTree >= 16) {
-    $("#" + sk_subtreeName).css("background-size", "99% 100%"); 
-  }
-  else if (pointsInTree >= 3) {
-    $("#" + sk_subtreeName).css("background-size", "99% 75%"); 
-  }
-  else if (pointsInTree >= 1) {
-    $("#" + sk_subtreeName).css("background-size", "99% 50%");     
-  }
-  else if (pointsInTree == 0) {
-    $("#" + sk_subtreeName).css("background-size", "99% 25%"); 
-  }
-};
-
-/**
- * Supposed to be triggered by clicking on an icon of a skill. 
- * This checks the state of the clicked skill and adds it basic or aced, accordingly to the state it is in. 
- * @param {Object} skillObj A jQuery object representing the clicked skill icon  
- */
-GUI.prototype.Skill_Add = function (skillObj) {
-  if (!skillObj.hasClass("sk_locked")) {
-    if (skillObj.hasClass("sk_selected_basic") || skillObj.hasClass("sk_selected_aced")) {
-      skillObj.removeClass("sk_selected_basic"); 
-      skillObj.addClass("sk_selected_aced"); 
     }
-    else {
-      skillObj.addClass("sk_selected_basic"); 
+
+    /** Change site title. Useful for naming builds, you can later find them easier in your history for example.
+     * @param {string} titleText Title to change to.
+     */
+    Title_ChangeTo(titleText) {
+        if (titleText) {
+            $("head title").text(titleText);
+        } else {
+            $("head title").text("Payday 2 Builder");
+        }
     }
+
+    /**
+     * Change selected Skill Tab to another.
+     * @param {string} containerId Id of the Tab to switch to 
+     */
+    Tab_ChangeTo(containerId) {
+        $("#sk_container").children(".sk_container").each(function () {
+            $(this).hide(); 
+        });
+        $("#" + containerId).show(); 
+    }
+
+    /**
+     * Raise or lower the subtree background according to points set in it.
+     * Does not yet support accurate moving (only at set thresholds)
+     * @param {string} sk_subtreeName Id of the subtree to move
+     * @param {number} pointsInTree Number of points to "move to"
+     */
+    Subtree_MoveBackground(sk_subtreeName, pointsInTree) {
+        if (pointsInTree >= 16)
+            $("#" + sk_subtreeName).css("background-size", "99% 100%"); 
+        else if (pointsInTree >= 3)
+            $("#" + sk_subtreeName).css("background-size", "99% 75%"); 
+        else if (pointsInTree >= 1)
+            $("#" + sk_subtreeName).css("background-size", "99% 50%");     
+        else if (pointsInTree == 0)
+            $("#" + sk_subtreeName).css("background-size", "99% 25%"); 
+    }
+
+    /**
+     * Supposed to be triggered by clicking on an icon of a skill. 
+     * This checks the state of the clicked skill and adds it basic or aced, accordingly to the state it is in. 
+     * @param {Object} skillObj A jQuery object representing the clicked skill icon  
+     */
+    Skill_Add(skillObj) {
+        if (!skillObj.hasClass("sk_locked")) {
+            if (skillObj.hasClass("sk_selected_basic") || skillObj.hasClass("sk_selected_aced")) {
+                skillObj.removeClass("sk_selected_basic"); 
+                skillObj.addClass("sk_selected_aced"); 
+            } else {
+                skillObj.addClass("sk_selected_basic"); 
+            }
+        
+        //Subtree_MoveBackground()
+        } 
+    }
+
+    /**
+     * Supposed to be triggered by clicking on an icon of a skill. 
+     * This checks the state of the clicked skill and removes it basic or aced, accordingly to the state it is in. 
+     * @param {Object} skillObj A jQuery object representing the clicked skill icon  
+     */
+    Skill_Remove(skillObj) {
+        if (skillObj.hasClass("sk_selected_aced")) {
+            skillObj.removeClass("sk_selected_aced"); 
+            skillObj.addClass("sk_selected_basic"); 
+        } else if (skillObj.hasClass("sk_selected_basic")) {
+            skillObj.removeClass("sk_selected_basic"); 
+        }
     
-    //Subtree_MoveBackground()
-  } 
-}
-
-/**
- * Supposed to be triggered by clicking on an icon of a skill. 
- * This checks the state of the clicked skill and removes it basic or aced, accordingly to the state it is in. 
- * @param {Object} skillObj A jQuery object representing the clicked skill icon  
- */
-GUI.prototype.Skill_Remove = function (skillObj) {
-  if (skillObj.hasClass("sk_selected_aced")) {
-    skillObj.removeClass("sk_selected_aced"); 
-    skillObj.addClass("sk_selected_basic"); 
-  }
-  else if (skillObj.hasClass("sk_selected_basic")) {
-    skillObj.removeClass("sk_selected_basic"); 
-  }
-  
-  //MoveBackground(sT, skills.pointsSpent[sT.split("_")[1]]); 
+        //MoveBackground(sT, skills.pointsSpent[sT.split("_")[1]]); 
+    }
 }
 
 const gui = new GUI();  
--- public/js/builder.main.js
+++ public/js/builder.main.js
@@ -1,53 +1,53 @@
 $(document).ready(function () {
 
-  //
-  // Bind Events on page 
+    //
+    // Bind Events on page 
   
-  // Skill tab navigation //
-  $("#sk_mastermind_tab").click(function () {
-    gui.Tab_ChangeTo("sk_mastermind_container"); 
-  });
-  $("#sk_enforcer_tab").click(function () {
-    gui.Tab_ChangeTo("sk_enforcer_container"); 
-  });
-  $("#sk_technician_tab").click(function () {
-    gui.Tab_ChangeTo("sk_technician_container"); 
-  });
-  $("#sk_ghost_tab").click(function () {
-    gui.Tab_ChangeTo("sk_ghost_container"); 
-  });
-  $("#sk_fugitive_tab").click(function () {
-    gui.Tab_ChangeTo("sk_fugitive_container"); 
-  });
+    // Skill tab navigation //
+    $("#sk_mastermind_tab").click(function () {
+        gui.Tab_ChangeTo("sk_mastermind_container"); 
+    });
+    $("#sk_enforcer_tab").click(function () {
+        gui.Tab_ChangeTo("sk_enforcer_container"); 
+    });
+    $("#sk_technician_tab").click(function () {
+        gui.Tab_ChangeTo("sk_technician_container"); 
+    });
+    $("#sk_ghost_tab").click(function () {
+        gui.Tab_ChangeTo("sk_ghost_container"); 
+    });
+    $("#sk_fugitive_tab").click(function () {
+        gui.Tab_ChangeTo("sk_fugitive_container"); 
+    });
 
-  // Skill Icon buttons //
-  $(".sk_icon").each(function () {
+    // Skill Icon buttons //
+    $(".sk_icon").each(function () {
     // Bind on left click
-    $(this).click(function () {
-      gui.Skill_Add($(this)); 
-      // Skill backend here
-    });
+        $(this).click(function () {
+            gui.Skill_Add($(this)); 
+            // Skill backend here
+        });
 
-    // Bind on right click
-    $(this).contextmenu(function (event) {
-      event.preventDefault(); 
+        // Bind on right click
+        $(this).contextmenu(function (event) {
+            event.preventDefault(); 
 
-      gui.Skill_Remove($(this)); 
-      // Skill backend here
-    });
+            gui.Skill_Remove($(this)); 
+            // Skill backend here
+        });
 
-    // Bind on hovering mouse, to show skill description
-    $(this).mouseover(function () {
-      $(".sk_description").css("visibility", "visible"); 
-    });
+        // Bind on hovering mouse, to show skill description
+        $(this).mouseover(function () {
+            $(".sk_description").css("visibility", "visible"); 
+        });
     
-    // Bind on mouse leave, to hide skill description
-    $(this).mouseleave(function () {
-      $(".sk_description").css("visibility", "hidden"); 
-    }); 
+        // Bind on mouse leave, to hide skill description
+        $(this).mouseleave(function () {
+            $(".sk_description").css("visibility", "hidden"); 
+        }); 
 
-  });
+    });
 
-  //$(".sk_points_remaining span").text(skills.pointsRemaining); 
-  gui.Tab_ChangeTo("sk_mastermind_container"); 
+    //$(".sk_points_remaining span").text(skills.pointsRemaining); 
+    gui.Tab_ChangeTo("sk_mastermind_container"); 
 });
