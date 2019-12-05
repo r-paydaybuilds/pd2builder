2775f9f2c44b8785b52ba8fa8f786202bcd66dba
ImUrX <urielfontan2002@gmail.com>
2019-12-05 14:37:53 -0300

    just making the local dev server host in index.html

--- pd2builder.js
+++ pd2builder.js
@@ -11,6 +11,7 @@ tools.use(bodyParser.json());
 
 tools.use("/", express.static("./public"));
 tools.get("/", (req, res) => res.sendFile("index.html", { root: "./pages/"}));
+tools.get("/index.html", (req, res) => res.sendFile("index.html", { root: "./pages/"}));
 tools.get("/bigoil.html",  (req, res) => res.sendFile("bigoil.html", { root: "./pages/"}));
 tools.get("/mobile.html",  (req, res) => res.sendFile("mobile.html", { root: "./pages/"}));
 tools.get("/mask.html",  (req, res) => res.sendFile("mask.html", { root: "./pages/"}));
