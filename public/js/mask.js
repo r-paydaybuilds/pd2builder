7bbb05fb983f4065edfd9e73069ed020540c13ae
ImUrX <urielfontan2002@gmail.com>
2020-02-27 00:03:19 -0300

    new gltf model better done

--- public/js/mask.js
+++ public/js/mask.js
@@ -3,7 +3,6 @@ import { GLTFLoader } from "https://unpkg.com/three@0.113.2/examples/jsm/loaders
 import MaskControls from "./MaskControls.js";
 
 const loader = new GLTFLoader().setPath("models/");
-const textures = new THREE.TextureLoader().setPath("models/");
 const canvas = document.getElementById("sim");
 const renderer = new THREE.WebGLRenderer({ 
     canvas,
@@ -53,10 +52,6 @@ function render() {
   
 loader.load("msk_alienware.gltf", gltf => {
     const scen = gltf.scene;
-    const mesh = scen.children[0].children[0];
-    const df = textures.load("alienware_df.png", () => render()), nm = textures.load("alienware_nm.png", () => render()), mc = textures.load("matcap_plastic_df.png", () => render());
-    df.flipY = false, nm.flipY = false, mc.flipY = false;
-    mesh.material = new THREE.MeshMatcapMaterial({ map: df, bumpMap: nm, bumpScale: 1, matcap: mc });
     scen.scale.set(5, 5, 5);
     scene.add(scen);
     const controls = new MaskControls(camera, canvas, scen);
